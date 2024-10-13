68ba8bdbdf4e42787794f18434c720bdb1759f3f
Himanshu10-09 <himanshuyv547@gmail.com>
2024-10-13 17:41:27 +0530

    Connected app with Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:password_vault_app/firebase_options.dart';
 import 'package:password_vault_app/presentation/home_page/home_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
