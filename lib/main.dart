d9fb29fd5a540d47d4ee72e1dd6a1dc9742ebdef
Himanshu10-09 <himanshuyv547@gmail.com>
2024-10-28 23:16:53 +0530

    Removed unused imports and comments

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:password_vault_app/firebase_options.dart';
-import 'package:password_vault_app/presentation/home_page/home_page.dart';
 import 'package:password_vault_app/presentation/login_page/auth.dart';
-import 'package:password_vault_app/presentation/login_page/login_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,7 +12,6 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
